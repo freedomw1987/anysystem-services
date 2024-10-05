5c5b3aaf09d86b49ee36846735fb632d7149368b
David @ Home <david@marsgroup.ltd>
2024-10-05 13:22:47 +0800

    init project

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,22 +1,41 @@
 import Bun from "bun";
 import moment from "moment";
 
+const port = process.env.PORT || 8000;
+
 Bun.serve({
-  port: process.env.PORT || 8000,
+  port,
 
   static: {
     "/health-check": Response.json({ status: "OK" }),
   },
 
-  fetch(req) {
-    console.log(
-      "Request @",
-      moment().format("YYYY-MM-DD HH:mm:ss"),
-      "|: ",
-      req.method,
-      req.url
-    );
+  fetch: (req) => {
+    try {
+      const url = new URL(req.url);
+      const method = req.method;
+      const apiEndpoint = `${method} ${url.pathname}`;
+      const params = url.searchParams;
 
-    return Response.json({ error: "Not found" }, { status: 404 });
+      console.log(apiEndpoint);
+
+      switch (apiEndpoint) {
+        case "GET /user":
+          return Response.json({ status: "user" });
+
+        case "GET /org":
+          return Response.json({ status: "org" });
+      }
+
+      return Response.json({ error: "Not found" }, { status: 404 });
+    } catch (error) {
+      return Response.json({ error: "Internal server error" }, { status: 500 });
+    }
   },
 });
+
+console.log(
+  `${moment().format(
+    "YYYY-MM-DD HH:mm:ss"
+  )}: Serving on http://localhost:${port}`
+);
